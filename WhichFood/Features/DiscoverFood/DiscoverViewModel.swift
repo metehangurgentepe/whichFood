011e75510519e91871f616a7ec031857006e2690
Mete <metehangurgentepe@gmail.com>
2024-03-04 00:14:39 +0300

    DiscoverFoodVC and DiscoverFoodViewModel created. CustomLoadingView added

---
+++ WhichFood/Features/DiscoverFood/DiscoverViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DiscoverViewModel.swift
+//  WhichFood
+//
+//  Created by Metehan Gürgentepe on 1.03.2024.
+//
+
+import Foundation
---
+++ WhichFood/Utils/WFError.swift
@@ -0,0 +1,8 @@
+//
+//  WFError.swift
+//  WhichFood
+//
+//  Created by Metehan Gürgentepe on 1.03.2024.
+//
+
+import Foundation
