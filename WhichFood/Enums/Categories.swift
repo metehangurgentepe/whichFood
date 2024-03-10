fa68b2552ea3c97eacae89be20f2c8e355c9e2ec
Mete <metehangurgentepe@gmail.com>
2024-03-10 10:08:22 +0300

    Added fav button to DetailRecipeVC

---
+++ WhichFood/Enums/Categories.swift
@@ -0,0 +1,8 @@
+//
+//  Categories.swift
+//  WhichFood
+//
+//  Created by Metehan Gürgentepe on 8.03.2024.
+//
+
+import Foundation
---
+++ WhichFood/Enums/Fonts.swift
@@ -0,0 +1,8 @@
+//
+//  Fonts.swift
+//  WhichFood
+//
+//  Created by Metehan Gürgentepe on 8.03.2024.
+//
+
+import Foundation
