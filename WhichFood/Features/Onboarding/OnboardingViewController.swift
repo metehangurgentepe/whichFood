f7a693f26920211acf1fb1b55880697072d36cfc
Mete <metehangurgentepe@gmail.com>
2024-09-20 11:32:27 +0300

    Add onboarding screen for first-time users

---
+++ WhichFood/Features/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingViewController.swift
+//  WhichFood
+//
+//  Created by Metehan Gürgentepe on 19.09.2024.
+//
+
+import Foundation
---
+++ WhichFood/Features/Onboarding/PageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PageViewController.swift
+//  WhichFood
+//
+//  Created by Metehan Gürgentepe on 19.09.2024.
+//
+
+import Foundation
